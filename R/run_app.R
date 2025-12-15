26002875628bfefada60faef36973c16a18eb47d
Dilân Olmo Şakar <dilan.olmo.sakar@gmail.com>
2025-12-15 01:17:02 +0100

    fix name

--- R/run_app.R
+++ R/run_app.R
@@ -2,7 +2,7 @@
 #' @export
 run_app <- function() {
   if (!requireNamespace("shiny", quietly = TRUE)) stop("Please install 'shiny'.")
-  app_dir <- system.file("app", package = "nutrientcalc")
+  app_dir <- system.file("app", package = "nutricalc")
   if (app_dir == "") stop("App directory not found inside the package.")
   shiny::runApp(app_dir, loadSupport = FALSE)
 }
