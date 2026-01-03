f11587e9771438601ec6e981619ea636ee32a5ec
dos <dilan.olmo.sakar@gmail.com>
2026-01-03 18:25:29 +0100

    Fix water mix slider label hiding

--- inst/app/app.R
+++ inst/app/app.R
@@ -260,7 +260,9 @@ ui <- fluidPage(
  .nutrient-row {border-bottom: 1px solid #dee2e6;padding: 2px 0 4px 0;margin-bottom: 2px; }
  .nutrient-row:last-child {border-bottom: none;}
 
- .water_mix_pct .irs-min,.water_mix_pct .irs-max,.water_mix_pct .irs-single {display: none !important;}
+ #water_mix_pct .irs-min,
+ #water_mix_pct .irs-max,
+ #water_mix_pct .irs-single { display: none !important; }
 
  .water-mix-table td { padding: 4px 6px; height: 37.8px; vertical-align: top; }
  .water-mix-table tr:last-child td { border-bottom: none !important; }
