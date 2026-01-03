0f832c53b3c22b9a7e750174cde05230be11c696
Dilân Olmo Şakar <dilan.olmo.sakar@gmail.com>
2026-01-03 23:22:20 +0100

    remove loadSupport

--- R/run_app.R
+++ R/run_app.R
@@ -1,10 +1,14 @@
 #' Launch the NutriCalc Shiny App
 #' @export
-run_app <- function() {
-  if (!requireNamespace("shiny", quietly = TRUE)) stop("Please install 'shiny'.")
+run_app <- function(...) {
+  if (!requireNamespace("shiny", quietly = TRUE)) {
+    stop("Please install 'shiny'.", call. = FALSE)
+  }
+
   app_dir <- system.file("app", package = "nutricalc")
-  if (app_dir == "") stop("App directory not found inside the package.")
-  shiny::runApp(app_dir, loadSupport = FALSE)
+  if (app_dir == "") {
+    stop("App directory not found inside the package.", call. = FALSE)
+  }
+
+  shiny::runApp(app_dir, ...)
 }
-
-
