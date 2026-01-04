5d4abd124375462bda2ed875c5bfcf1d37c33ab4
dos <dilan.olmo.sakar@gmail.com>
2026-01-04 02:57:05 +0100

    Ensure bslib is checked in run_app

--- R/run_app.R
+++ R/run_app.R
@@ -5,11 +5,15 @@ run_app <- function(...) {
   if (!requireNamespace("shiny", quietly = TRUE)) {
     stop("Please install 'shiny'.", call. = FALSE)
   }
+  if (!requireNamespace("bslib", quietly = TRUE)) {
+    stop("Please install 'bslib'.", call. = FALSE)
+  }
 
   app_dir <- system.file("app", package = "nutricalc")
   if (app_dir == "") {
     stop("App directory not found inside the package.", call. = FALSE)
   }
 
+  invisible(bslib::bs_theme())
   shiny::runApp(app_dir, ...)
 }
