f62f4a2ea1b33ed76be03d45ec609add2ddb119d
Dilân Olmo Şakar <dilan.olmo.sakar@gmail.com>
2026-01-04 02:40:52 +0100

    delepe update percentage matrix

--- data-raw/update_percentage_matrix.R
+++
@@ -1,8 +0,0 @@
-#' Update and save the percentage matrix
-#'
-#' @importFrom usethis use_data
-update_percentage_matrix <- function() {
-  percentage_matrix <- compute_percentage_matrix(nutrient_matrix)
-  usethis::use_data(percentage_matrix, overwrite = TRUE)
-  message("\u2705 percentage_matrix updated and saved.")
-}
